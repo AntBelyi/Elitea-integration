f226e0ba01a8dcec971734a1ddc7c8efcc62c612
AntBelyi <66726924+AntBelyi@users.noreply.github.com>
2026-09-14 11:02:59 +0300

    Create src/test/java/tests_folder/EpamClientWorkTest.java

---
+++ src/test/java/tests_folder/EpamClientWorkTest.java
@@ -0,0 +1,44 @@
+package tests_folder;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import junit.framework.TestCase;
+
+public class EpamClientWorkTest extends TestCase {
+
+    private Playwright playwright;
+    private Browser browser;
+    private Page page;
+
+    @Override
+    protected void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        page = browser.newPage();
+    }
+
+    @Override
+    protected void tearDown() {
+        if (browser != null) {
+            browser.close();
+        }
+        if (playwright != null) {
+            playwright.close();
+        }
+    }
+
+    public void testClientWorkIsVisible() {
+        try {
+            page.navigate("https://www.epam.com/");
+            page.locator("a.top-navigation__item-link.js-op[href=\"/services\"]").click();
+            page.locator("a[href=\"https://www.epam.com/services/client-work\"]").click();
+
+            assertTrue(page.getByRole(com.microsoft.playwright.options.AriaRole.HEADING,
+                    new Page.GetByRoleOptions().setName("Client Work")).isVisible());
+        } finally {
+            tearDown();
+        }
+    }
+}
